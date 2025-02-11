32d8ed2630e07c9f46c299aca3ed029521e84c27
Eric Trotchie <ejtrotchie@gmail.com>
2025-02-11 03:38:40 -0700

    Checking if there is a user

--- app/(banking)/account/page.tsx
+++ app/(banking)/account/page.tsx
@@ -2,14 +2,17 @@
 import BankSignUpForm from "@/components/BankSignUpForm";
 import UserDetails from "@/components/UserDetails";
 import { getCurrentUser } from "@/lib/actions/user.actions";
+import { redirect } from "next/navigation";
 
 
 async function page() {
   const currentUser = await getCurrentUser();
+   if (!currentUser) return redirect('/sign-in');
 
-
+  // Remove dwolla from customer, switching to stripe
   return (
     <>
+    
       {!currentUser.dwollaCustomerId ? (
         <BankSignUpForm userId={currentUser.$id as string}/>
       ): <UserDetails {...currentUser} />}
